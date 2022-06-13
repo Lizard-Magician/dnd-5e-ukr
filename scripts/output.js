b3a40966a6b9f3fa29ed0b0b5f629b72c03d261f
Shylo-Ivan-MIT <shikon2001@gmail.com>
2022-06-13 22:06:46 +0300

    quick update for showing spells before searching

---
+++ scripts/output.js
@@ -0,0 +1,24 @@
+var data = JSON.parse(magic);
+let listHolder = document.querySelector('#list-holder');
+listHolder.innerHTML = ""
+
+
+for (i = 0; i < data.length; i++) {
+    let obj = data[i];
+
+    const elem0 = document.createElement("dt")
+    const elem1 = document.createElement("dt")
+    const elem2 = document.createElement("dd")
+
+    elem0.innerHTML = `${obj.NameUkr} - [ ${obj.Name} ] `
+    elem1.innerHTML = `${obj.Level}, Школа: ${obj.School} 
+        <br> Час створення: ${obj.Casting_time} 
+        <br> Дистанція:${obj.Distance} 
+        <br> Компоненти: <br> ${obj.Components} 
+        <br> Тривалість: ${obj.Duration}`
+    elem2.innerHTML = `${obj.Description}`
+    listHolder.appendChild(elem0)
+    listHolder.appendChild(elem1)
+    listHolder.appendChild(elem2)
+
+}
